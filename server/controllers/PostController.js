37b01d7ae6561d621ad1508e302957083d7e226d
Henrry Tien (Dao Tien) <58262966+DTien93@users.noreply.github.com>
2023-07-22 23:08:47 +0700

    Post Controller

--- server/controllers/PostController.js
+++ server/controllers/PostController.js
@@ -1,2 +1,28 @@
 import PostModel from "../models/PostModel";
+import messageModel from "../models/messageModel";
 
+export const addMessage = async (req, res) => {
+    const { chatId, senderId, text } = req.body;
+    const message = new MessageModel({
+        chatId,
+        senderId,
+        text,
+    });
+    try {
+        const result = await message.save();
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+};
+
+export const getMessages = async (req, res) => {
+    const { chatId } = req.params;
+
+    try {
+        const result = await messageModel.find({ chatId });
+        res.status(200).json(result);
+    } catch (e) {
+        res.status(500).json(e);
+    }
+}
